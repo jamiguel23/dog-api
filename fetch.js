27f55843067fb31cddb49b7e577cd37de55af5aa
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2023-05-03 11:15:20 -0700

    worked on lab

---
+++ fetch.js
@@ -0,0 +1,9 @@
+async function logJSONData() {
+  const response = await fetch("https://dog-api.kinduff.com/api/facts?number=5");
+  const jsonData = await response.json();
+  console.log(jsonData);
+}
+
+logJSONData()
+
+//console.log(jsonData);ﬁ
